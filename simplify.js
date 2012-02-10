19e6bff23e381ec7eaeafeb8b714a2dcab934744
mourner <agafonkin@gmail.com>
2012-02-10 16:24:09 +0200

    some comment magic

--- simplify.js
+++ simplify.js
@@ -9,100 +9,62 @@
 	"use strict";
 
 
-	// run search/replace for '.x' and '.y' to suit your point format
-	// (its configurability would draw significant performance overhead)
+	// to suit your point format, run search/replace for '.x' and '.y'
+	// to switch to 3D, uncomment the lines in the next 2 functions
+	// (configurability would draw significant performance overhead)
 
 	function getSquareDistance(p1, p2) { // square distance between 2 points
 
 		var dx = p1.x - p2.x,
+	//		dz = p1.z - p2.z,
 		    dy = p1.y - p2.y;
 
-		return dx * dx + dy * dy;
+		return dx * dx +
+	//	       dz * dz +
+		       dy * dy;
 	}
 
 	function getSquareSegmentDistance(p, p1, p2) { // square distance from a point to a segment
 
 		var x = p1.x,
 		    y = p1.y,
+	//	    z = p1.z,
 
 		    dx = p2.x - x,
 		    dy = p2.y - y,
+	//	    dz = p2.z - z,
 
 		    t;
 
 		if (dx !== 0 || dy !== 0) {
 
 			t = ((p.x - x) * dx +
+	//		     (p.z - z) * dz +
 			     (p.y - y) * dy) /
-			    (dx * dx + dy * dy);
+			        (dx * dx +
+	//		         dz * dz +
+			         dy * dy);
 
 			if (t > 1) {
 				x = p2.x;
 				y = p2.y;
+	//			z = p2.z;
 
 			} else if (t > 0) {
 				x += dx * t;
 				y += dy * t;
+	//			z += dz * t;
 			}
 		}
 
 		dx = p.x - x;
 		dy = p.y - y;
+	//	dz = p.z - z;
 
-		return dx * dx + dy * dy;
+		return dx * dx +
+	//	       dz * dz +
+		       dy * dy;
 	}
-
-
-	// replace previous functions with the following for 3D space
-
-	/*
-	function getSquareDistance(p1, p2) {
-
-		var dx = p1.x - p2.x,
-		    dy = p1.y - p2.y,
-		    dz = p1.z - p2.z;
-
-		return dx * dx + dy * dy + dz * dz;
-	}
-
-	function getSquareSegmentDistance(p, p1, p2) {
-
-		var x = p1.x,
-		    y = p1.y,
-		    z = p1.z,
-
-		    dx = p2.x - x,
-		    dy = p2.y - y,
-		    dz = p2.z - z,
-
-		    t;
-
-		if (dx !== 0 || dy !== 0) {
-
-			t = ((p.x - x) * dx +
-			     (p.y - y) * dy +
-			     (p.z - z) * dz) /
-			    (dx * dx + dy * dy + dz * dz);
-
-			if (t > 1) {
-				x = p2.x;
-				y = p2.y;
-				z = p2.z;
-
-			} else if (t > 0) {
-				x += dx * t;
-				y += dy * t;
-				z += dz * t;
-			}
-		}
-
-		dx = p.x - x;
-		dy = p.y - y;
-		dz = p.z - z;
-
-		return dx * dx + dy * dy + dz * dz;
-	}
-	*/
 
 	// the rest of the code doesn't care for the point format
 
