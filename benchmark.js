0728d0ac532052e14233914f97297279a4473b13
Vladimir Agafonkin <agafonkin@gmail.com>
2013-10-16 14:26:02 +0300

    remove leftover

--- benchmark.js
+++ benchmark.js
@@ -125,53 +125,6 @@ function oneStack(points, sqTolerance) {
     return newPoints;
 }
 
-function oneStack(points, sqTolerance) {
-
-    var len = points.length,
-        MarkerArray = (typeof Uint8Array !== undefined + '') ? Uint8Array : Array,
-        markers = new MarkerArray(len),
-
-        first = 0,
-        last = len - 1,
-
-        stack = [],
-        newPoints = [],
-
-        i, maxSqDist, sqDist, index;
-
-    markers[first] = markers[last] = 1;
-
-    while (last) {
-
-        maxSqDist = 0;
-
-        for (i = first + 1; i < last; i++) {
-            sqDist = getSquareSegmentDistance(points[i], points[first], points[last]);
-
-            if (sqDist > maxSqDist) {
-                index = i;
-                maxSqDist = sqDist;
-            }
-        }
-
-        if (maxSqDist > sqTolerance) {
-            markers[index] = 1;
-            stack.push(first, index, index, last);
-        }
-
-        last = stack.pop();
-        first = stack.pop();
-    }
-
-    for (i = 0; i < len; i++) {
-        if (markers[i]) {
-            newPoints.push(points[i]);
-        }
-    }
-
-    return newPoints;
-}
-
 
 function oneStackCalvin(points, sqTolerance) {
 
