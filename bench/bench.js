2a252bd2608c28c5f46a7a8a8b7b6d4388051612
Vladimir Agafonkin <agafonkin@gmail.com>
2015-03-23 13:02:42 +0200

    add a bench script

---
+++ bench/bench.js
@@ -0,0 +1,17 @@
+
+var Benchmark = require('benchmark');
+var simplify = require('../simplify');
+
+var points = require('../test/fixtures/74k');
+
+new Benchmark.Suite()
+.add('simplify (HQ)', function() {
+    simplify(points, 1, true);
+})
+.add('simplify', function() {
+    simplify(points, 1, false);
+})
+.on('cycle', function(event) {
+    console.log(String(event.target));
+})
+.run();
