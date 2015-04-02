b275c45883aa3314076c6ccf48bac27ffe017050
Vladimir Agafonkin <agafonkin@gmail.com>
2015-04-02 12:33:09 +0300

    use smaller fixture for benchmarks

--- bench/bench.js
+++ bench/bench.js
@@ -2,7 +2,7 @@
 var Benchmark = require('benchmark');
 var simplify = require('../simplify');
 
-var points = require('../test/fixtures/74k');
+var points = require('../test/fixtures/1k.json');
 
 new Benchmark.Suite()
 .add('simplify (HQ)', function() {
